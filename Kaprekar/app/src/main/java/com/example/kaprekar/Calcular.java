7bc24f58f6a20ce2cfb544f80ffb23a38c1cf206
EdwardJimenez <jve0021477@est.univalle.edu>
2022-06-17 13:47:28 -0400

    Metodos kaprekar 100%

--- Kaprekar/app/src/main/java/com/example/kaprekar/Calcular.java
+++ Kaprekar/app/src/main/java/com/example/kaprekar/Calcular.java
@@ -1,15 +1,23 @@
 package com.example.kaprekar;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.sax.Element;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class Calcular extends AppCompatActivity {
 
-    Button btnRegresar;
+    Button btnRegresar,btnLimpiar,btnRutinaKaprekar;
+    EditText etNumero, etIteracion, etLista;
+    Metodos metodos = new Metodos();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +25,12 @@ public class Calcular extends AppCompatActivity {
         setContentView(R.layout.activity_calcular);
 
         btnRegresar = findViewById(R.id.btnRegresar2);
-
-
+        btnRutinaKaprekar = findViewById(R.id.btnRutinaKaprekar);
+        btnLimpiar = findViewById(R.id.btnLimpiar);
+        etNumero = findViewById(R.id.etNumero);
+        etIteracion = findViewById(R.id.etIteacion);
+        etLista = findViewById(R.id.etLista);
+        
         btnRegresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,5 +39,36 @@ public class Calcular extends AppCompatActivity {
             }
         });
 
+        btnRutinaKaprekar.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    String numero = etNumero.getText().toString();
+
+                    ArrayList<String> listado = metodos.kaprekarMostrar(numero);
+                    int iteracion = metodos.getCount();
+
+                    etIteracion.append(Integer.toString(iteracion));
+
+                    for (int i=0; i<listado.stream().count();i++)
+                    {
+                        etLista.append(listado.get(i));
+                    }
+                }catch (Exception ex){
+
+                }
+            }
+        });
+
+        btnLimpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                etIteracion.setText("");
+                etNumero.setText("");
+                etLista.setText("");
+            }
+        });
     }
 }
